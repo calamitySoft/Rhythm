ad9513b617a1db11c752c341e8a6a75d29ba9bd6
Logan Moseley <git@loganmoseley.com>
2015-05-17 12:25:46 -0400

    Color pulseView like attack/block/throw views

--- Rhythm/ViewController.m
+++ Rhythm/ViewController.m
@@ -23,6 +23,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.pulseView.backgroundColor = [[self class] highlightColor];
     self.pulseView.layer.cornerRadius = CGRectGetWidth(self.pulseView.frame) / 2;
 }
 
